226160f043aa1066fdd827a69f735bab242c187c
Edward Tanguay (windows) <edward@tanguay.info>
2025-05-29 15:19:42 +0200

    compare dog

--- classes/dog.rb
+++ classes/dog.rb
@@ -9,7 +9,17 @@ class Dog
 		age * 7
 	end
 
-  def is_older_than(age)
-    self.age > age
-  end
+	def is_older_than(age)
+		self.age > age
+	end
+
+	def compare_age(dog)
+			if(self.age > dog.age)
+				"#{self.name} is #{self.age} and so is older than #{dog.name} who is #{dog.age}."
+			elsif (self.age < dog.age)
+				"#{self.name} is #{self.age} and so is younger than #{dog.name} who is #{dog.age}."
+			else 
+				"#{self.name} and #{dog.name} are both #{self.age}."
+			end
+	end
 end
--- main.rb
+++ main.rb
@@ -8,10 +8,18 @@ dog2= Dog.new
 dog2.name = "Spot"
 dog2.age = 8
 
+dog3= Dog.new
+dog3.name = "Rocky"
+dog3.age = 8
+
 puts dog1.get_info
 puts dog2.get_info
 puts dog1.is_older_than(5)
 puts dog2.is_older_than(5)
+
+puts dog1.compare_age(dog2)
+puts dog2.compare_age(dog1)
+puts dog2.compare_age(dog3)
 
 
 
