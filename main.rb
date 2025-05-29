80fb4d7d6b36f46079297ea8eb553c2c603b58e1
Edward Tanguay (windows) <edward@tanguay.info>
2025-05-29 12:20:48 +0200

    two

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ class Dog
 	attr_accessor :name, :age
 	
 	def get_info
-		"#{name} is #{age} years old."
+		"#{name} is #{age} years old which is #{self.age_in_human_years} in human years."
 	end
 
 	def age_in_human_years
@@ -10,12 +10,16 @@ class Dog
 	end
 end
 
-dog = Dog.new
-dog.name = "Fido"
-dog.age = 3
+dog1= Dog.new
+dog1.name = "Fido"
+dog1.age = 3
 
-puts dog.get_info
-puts "#{dog.name} is #{dog.age_in_human_years} in human years."
+dog2= Dog.new
+dog2.name = "Spot"
+dog2.age = 8
+
+puts dog1.get_info
+puts dog2.get_info
 
 
 
