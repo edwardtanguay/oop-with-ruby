4d8b47de1145b7f1b4eff2bd5079becfd3097d4b
Edward Tanguay (windows) <edward@tanguay.info>
2025-05-29 11:34:58 +0200

    method1

--- main.rb
+++ main.rb
@@ -1,13 +1,16 @@
 class Dog
 	attr_accessor :name, :age
+	
+	def get_info
+		"#{name} is #{age} years old."
+	end
 end
 
 dog = Dog.new
 dog.name = "Fido"
 dog.age = 3
 
-puts dog.name
-puts dog.age
+puts dog.get_info
 
 
 
